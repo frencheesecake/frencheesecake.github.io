c65e63b263c4dc1ae0eeac39ef8737320bb97f07
cheesecake <fran.chez.polidario@gmail.com>
2024-10-06 00:37:18 +0800

    Initial commit.

---
+++ dist/script/main.js
@@ -0,0 +1,56 @@
+// Create timeline
+const tl = gsap.timeline();
+
+// ⭐Title text reveal animation⭐
+let text = document.querySelector('#text');
+let words = text.textContent.split(" ");
+
+text.innerHTML = ""; // Clear element
+
+words.forEach(word => {
+let span = document.createElement("span");
+span.textContent = word + " ";
+text.appendChild(span);
+});
+
+tl.from("h1 span", {
+opacity: 0,
+y: 50,
+stagger: 0.05,
+duration: 1
+});
+
+// ⭐Text and Image gallery reveal animation⭐
+const gridChildren = document.querySelectorAll('.grid > div');
+
+tl.from(gridChildren, {
+    opacity: 0,
+    y: 50,         
+    stagger: 0.05,   
+    duration: 1,   
+    ease: "power4.out"
+});
+
+
+// ⭐Interactive background gradient⭐
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector('.interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    const move = () => {
+        curX += (tgX - curX) / 10;
+        curY += (tgY - curY) / 10;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(move);
+    };
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+});
